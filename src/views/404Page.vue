77e62398fd81b8f742efaca96d323911c9d63247
deng zi wen <1967629423@qq.com>
2019-08-03 03:55:09 +0800

    404 style change

--- src/views/404Page.vue
+++ src/views/404Page.vue
@@ -8,31 +8,32 @@
         </div>
         <div class="footer">
             <div class="introduce">
-                <p>"Powered by" <a href="www.cdlk.tk">CDLK.TK</a></p>
+                <p>
+                    "Powered by"
+                    <a href="www.cdlk.tk">CDLK.TK</a>
+                </p>
             </div>
             <div class="action">
                 <a href="javascript:history.go(-1)">返回上一页</a>
             </div>
-            
         </div>
     </div>
 </template>
 <style lang="scss">
-$baseFontColor: rgb(130,130,130);
-$descFontColor: $baseFontColor * rgb(1.35,1.01,1.01);
-@mixin text-shadow($range:4px)
-{
+$baseFontColor: rgb(130, 130, 130);
+$descFontColor: $baseFontColor * rgb(1.35, 1.01, 1.01);
+@mixin text-shadow($range: 4px) {
     text-shadow: 0px 0px $range rgba(0, 0, 0, 0.663);
 }
-@mixin radius-button($radius: 4px)
-{
+@mixin radius-button($radius: 4px) {
     padding: 6px 20px;
     margin: 6px;
     border: 1px solid $descFontColor;
     border-radius: $radius;
 }
-a , a:link , a:visited
-{
+a,
+a:link,
+a:visited {
     color: $descFontColor;
 }
 html,
@@ -64,26 +65,26 @@ body {
             @include text-shadow();
         }
     }
-    .footer
-    {
+    .footer {
         @include text-shadow(1px);
         display: flex;
         flex-direction: column-reverse;
         position: absolute;
         bottom: 20px;
         width: 100%;
-        .action
-        {
-            @include radius-button();
-            width: 120px;
-            margin: 0 auto;
-            a
-            {
+        .action {
+            a {
+                @include radius-button();
+                width: 120px;
+                margin: 0 auto;
                 text-decoration: none;
             }
+            a:active
+            {
+                background-color:$descFontColor * rgb(0.4,0.4,0.4);
+            }
         }
-        .introduce
-        {
+        .introduce {
             padding-top: 30px;
             color: $descFontColor;
         }
