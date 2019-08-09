e836edbfcba0347b17705fc9938d84cfa33803af
deng zi wen <1967629423@qq.com>
2019-08-10 00:30:32 +0800

    index style change

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,47 +1,47 @@
 <template>
     <div class="index-content">
-        <link href="//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+        <link
+            href="//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
+            rel="stylesheet"
+        />
         <CubeLoop></CubeLoop>
         <div class="index-head">
-            <div class="index-head-text">
-                Welcome
-            </div>
+            <div class="index-head-text">Welcome</div>
         </div>
         <div class="index-arrowfather">
-        <div class="index-arrow" id="arrow1">
-        <a class="index-d1" href="#"><i class="fa fa-angle-double-right fa-3x" ></i></a>
-        </div>
+            <div class="index-arrow" id="arrow1">
+                <a class="index-d1" href="#">
+                    <i class="fa fa-angle-double-right fa-3x"></i>
+                </a>
+            </div>
         </div>
     </div>
 </template>
 <style lang="scss">
-.index-arrowfather{
+.index-arrowfather {
     position: relative;
-    .index-d1{
+    height: 100vh;
+    .index-d1 {
         color: rgb(128, 123, 123);
     }
-    .index-arrow{
-        left:95%;
-        position: fixed;
+    .index-arrow {
+        right: 30px;
+        position: absolute;
         top: 50%;
+        transform: translate(-50%, -50%);
     }
-
 }
-.index-content
-{
+.index-content {
     text-align: center;
-    .index-head
-    {
+    .index-head {
         position: absolute;
         width: 330px;
         height: 90px;
-        bottom: 50%;
+        top: 50%;
         left: 50%;
-        margin-bottom: -45px;
-        margin-left: -165px;
+        transform: translate(-50%, -50%);
     }
-    .index-head-text
-    {
+    .index-head-text {
         display: inline;
         font-size: 80px;
     }
@@ -51,12 +51,9 @@
 </style>
 
 <script lang="ts">
-import {Vue,Component} from 'vue-property-decorator'
+import { Vue, Component } from 'vue-property-decorator'
 import CubeLoop from '../components/background/CubeLoop.vue'
 import testBack from '../components/background/testBack.vue'
-@Component({components:{CubeLoop,testBack}})
-export default class Index extends Vue
-{
-
-}
+@Component({ components: { CubeLoop, testBack } })
+export default class Index extends Vue {}
 </script>
