8109acc8784cb7aed66ca277f349ddb7124ea0fb
ruan115 <1154347256@qq.com>
2019-08-15 16:35:01 +0800

    11

--- src/components/Card.modul.vue
+++ src/components/Card.modul.vue
@@ -1,17 +1,18 @@
 <template>
-<div class="modo-a" data-reactid="">
-    <div class="section-eid" data-reactid="">
-    <div data-reactid=" "></div>
-    <div class="" data-reactid="">
-    <a class="" href="" action="" data-reac>
-    <div data-reactid="">
-        <div class="" data-reactid="">
-            <img src="" alt="post" data-reactid="">
+<div class="modo-father">
+    <CubeLoop></CubeLoop>
+    <div>
+    <div class="modo-wrapper-a">
+    <div class="modo-wrapper-b">
+    <a class="post-tile-a" href="" action="Click">
+    <div class="modo-c">
+        <div class="modo-postimg">
+            <img src="../assets/images/icon/1.jpg" alt="post" class="modo-img">
         </div>
-        <div class="" data-reactid="">
-            <span class="" data-reactid="" >时间</span>
-            <h1 class="" data-reactid="">Introducing React Hooks</h1>
-            <span class="" data-reactid="">
+        <div class="modo-postinfo">
+            <span class="modo-time" >时间</span>
+            <h4 class="modo-h4">这是图片</h4>
+            <span class="modo-post-description">
                 简介
             </span>
         </div>
@@ -19,5 +20,56 @@
     </a>
     </div>
     </div>
+    </div>
 </div>
 </template>
+<style lang="scss">
+.modo-father{
+    display: flex;
+.modo-img{
+    max-width: 300px;
+    max-height: 200px;
+    border-top-left-radius: 23px;
+    border-top-right-radius: 23px;
+    z-index: abs($number: 200)
+    }
+.modo-postimg{
+    transition:width 2s;
+    -webkit-transition: width 2s;
+}
+.modo-c{
+    border-style: solid;
+    border-color: black;
+    position: absolute;
+    margin-left: 100px;
+    margin-top: 200px;
+    border-style: ridge;
+    border-radius: 25px;
+    box-shadow: 10px 10px 10px #888888
+}
+.modo-postinfo{
+    margin-top: 50px;
+    color: rgb(122, 56, 56);
+    flex-wrap: wrap;
+    max-width: 300px;
+    max-height: 200px;
+    overflow: hidden;
+    
+}
+// .modo-wrapper-b :hover{
+//     max-width: 310px;
+    
+// }
+.modo-c :hover{
+    max-width: 310px;
+
+}
+}
+</style>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import CubeLoop from '../components/background/CubeLoop.vue'
+@Component({name:'card',components:{ CubeLoop }})
+export default class card extends Vue {}
+</script>
+
--- src/views/Index.vue
+++ src/views/Index.vue
@@ -1,10 +1,11 @@
 <template>
     <div class="index-content">
+        
         <link
             href="//netdna.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css"
             rel="stylesheet"
-        />
-        <CubeLoop></CubeLoop>
+        /> <CubeLoop></CubeLoop>
+       <card></card>
         <div class="index-head">
             <div class="index-head-text">Welcome</div>
         </div>
@@ -54,6 +55,7 @@
 import { Vue, Component } from 'vue-property-decorator'
 import CubeLoop from '../components/background/CubeLoop.vue'
 import testBack from '../components/background/testBack.vue'
-@Component({ components: { CubeLoop, testBack } })
+import card from '../components/Card.modul.vue'
+@Component({ components: { CubeLoop, testBack ,card} })
 export default class Index extends Vue {}
 </script>
